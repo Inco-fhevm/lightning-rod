0e56145c61e0532d649ad08c5664bc2cbffa8cd5
Silas Davis <silas@inco.network>
2025-07-16 13:05:27 +0200

    feat: Upgrade to v0.2.17
    Signed-off-by: Silas Davis <silas@inco.network>

--- backend/src/test/lightning.local.e2e.test.ts
+++ backend/src/test/lightning.local.e2e.test.ts
@@ -3,11 +3,13 @@ import { anvil } from 'viem/chains';
 import { describe } from 'vitest';
 import { runE2ETest } from './lightning-test.ts';
 
+const anvilDefaultPrivateKey2 = '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d';
+
 describe(`Lightning Local Node E2E`, { timeout: 50_000 }, async () => {
   const zap = Lightning.localNode();
   runE2ETest(Math.floor(Math.random() * 100), zap, {
     chain: anvil,
-    senderPrivKey: zap.deployment.senderPrivateKey,
+    senderPrivKey: anvilDefaultPrivateKey2,
     hostChainRpcUrl: 'http://127.0.0.1:8545',
   });
 });
